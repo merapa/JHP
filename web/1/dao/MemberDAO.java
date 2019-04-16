19370c37cae86aab94961ca3bd12a900c524b441
홍지원 <44790940+Gomgae@users.noreply.github.com>
2019-04-16 15:13:57 +0900

    Update MemberDAO.java

--- web/1/dao/MemberDAO.java
+++ web/1/dao/MemberDAO.java
@@ -1,20 +1,36 @@
 package dao;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import model.Member;
 
 
 public class MemberDAO {
+
 	
-	private MemberDAO() {}
+	private MemberDAO() {
+	}
 
 	private static MemberDAO instance = new MemberDAO();
+	private String id;
+	private String pass;
+	private String name;
+	private String email;
+	private String question;
+	private String contact;
+	private String answer;
+	private int n;
+	private Connection con;
+	private PreparedStatement ps;
 	
 	// static 통해서 생성할 수 있도록 만들기
 	static public MemberDAO getInstance() {
@@ -34,19 +50,65 @@ public class MemberDAO {
 		}
 		return con;
 	}
+	
+	// -----------------------------------------------------------------
+	
+	// 회원 가입
+	protected Connection service(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		request.setCharacterEncoding("UTF-8");
+		setId(request.getParameter("id"));
+		setPass(request.getParameter("pass"));
+		setName(request.getParameter("name"));
+		setEmail(request.getParameter("email"));
+		setContact(request.getParameter("contact"));
+		setQuestion(request.getParameter("question"));
+		setAnswer(request.getParameter("answer"));
+		
+		setN(0);
+		setPs(null);
+		setCon(null);
+		
+		Connection con = null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("java:comp/env/jdbc/jhp");
+			con = ds.getConnection();
+			ps=con.prepareStatement("INSERT INTO member VALUES (?,?,?,?,?,?)");
+	  		ps.setString(1,id);
+	  		ps.setString(2,pass);
+	  		ps.setString(3,name);
+	  		ps.setString(4,email);
+	  		ps.setString(5,contact);
+	  		ps.setString(6,question);
+	  		ps.setString(7, answer);
+	  		
+	  		int result=ps.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	
+	
+	
+	
+	
 	// -----------------------------------------------------------------
 	
 	// 아이디 중복 확인
-	public int confirm(String id) {
-		String sql = "select id from jhp where id=?";
+	public int confirm(String id) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps =null;
 		ResultSet rs = null;
 		int result = -1;
 		
 		try {
+			ps=con.prepareStatement("select id from jhp where id=?");
 			con = getConnection();
-			ps = con.prepareStatement(sql);
+			ps = con.prepareStatement(id);
 			ps.setString(1, id);
 			rs=ps.executeQuery();
 			if(rs.next()) {
@@ -69,15 +131,15 @@ public class MemberDAO {
 // -----------------------------------------------------------------
 	// 멤버 수정
 	
-	public int updateMember(Member vo) {
+	public int updateMember(Member vo) throws SQLException {
 		int result = -1;
-		String sql = "update user set pass=?,email=?,email1=?,contact=?,question=?,answer=? where id=?";
+		ps=con.prepareStatement("update user set pass=?,email=?,email1=?,contact=?,question=?,answer=? where id=?");
 		Connection con = null;
 		PreparedStatement ps = null;
 		
 		try {
 			con=getConnection();
-			ps = con.prepareStatement(sql);
+			ps = con.prepareStatement(id);
 			ps.setString(1, vo.getpass());
 			ps.setString(2, vo.getemail());
 			ps.setString(3, vo.getemail());
@@ -99,13 +161,13 @@ public class MemberDAO {
 	
 // -----------------------------------------------------------------
 		// 아이디 중복 검사
-	public int idCheck(Connection con, String id) throws Exception{
+	public int idCheck(Connection con, String id) throws Exception,SQLException{
 		  PreparedStatement ps = null; 
 		  ResultSet rs = null;
-		  String sql = "select id from member where id=?";
+		  ps=con.prepareStatement("select id from member where id=?");
 		  
 		  try{
-		   ps = con.prepareStatement(sql);
+		   ps = con.prepareStatement(id);
 		   ps.setString(1, id);
 		   rs = ps.executeQuery();
 		   if (rs.next()) {
@@ -120,9 +182,18 @@ public class MemberDAO {
 		return -1;
 	}
 	// -----------------------------------------------------------------
-			// 
+			// 회원 가입
 	
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
@@ -151,8 +222,88 @@ public class MemberDAO {
 	}
 
 	public void insertMember(Member member) {
-		// TODO Auto-generated method stub
+
 		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public int getN() {
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	public PreparedStatement getPs() {
+		return ps;
+	}
+
+	public void setPs(PreparedStatement ps) {
+		this.ps = ps;
 	}
 	
 	
