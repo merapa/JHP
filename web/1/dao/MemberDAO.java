16726186fce3a686bc6fa12c3ad2924b25f59f90
홍지원 <44790940+Gomgae@users.noreply.github.com>
2019-04-17 15:20:03 +0900

    Update MemberDAO.java

--- web/1/dao/MemberDAO.java
+++ web/1/dao/MemberDAO.java
@@ -1,228 +1,186 @@
 package dao;
 import java.sql.*;
-import java.util.*;
+import util.*;
 import javax.naming.*;
 import javax.sql.*;
+
+import dto.MemberDTO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import model.Member;
 
 
-
 public class MemberDAO {
+	
 	Connection con = null;
 	PreparedStatement ps = null;
 	ResultSet rs = null;
-	
-	Member member = new Member();
-	private int result;
-	
-	//dbcp 만들기
-	public MemberDAO() {
-		
+	// ---------------------------------------------------------------------------------
+	public int login(String id,String pass) throws SQLException {
+		Connection con = DatabaseUtil.getConnection();
+		ps = con.prepareStatement("select * form user where = id");
 		try {
-			Context init = new InitialContext();
-			DataSource ds = (DataSource)init.lookup("java:comp/env/jdbc/jhp");
-			con = ds.getConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void dbClose() {
-		if(con != null)
-			try {
-				con.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-	}
-	
-	// -----------------------------------------------------------------
-	
-	// 회원 가입
-	public void MemberInsert(Member member) {
-		result = 0;
-		try {
-			ps = con.prepareStatement("insert into user value (?, ?, ?, ?, ?, ?, ?)");
-
-			member.setid(rs.getString("id"));
-			member.setpass(rs.getString("pass"));
-			member.setname(rs.getString("name"));
-			member.setemail(rs.getString("email"));
-			member.setcontact(rs.getString("contact"));
-			member.setquestion(rs.getString("question"));
-			member.setanswer(rs.getString("answer"));
-	  		
-	  		result = ps.executeUpdate();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (rs != null) try { rs.close();} catch (Exception e) {e.printStackTrace();}
-			if (ps != null) try { ps.close(); } catch (Exception e) {e.printStackTrace();}
-		}
-		
-		return;
-		
-	}
-	
-	
-	
-	
-	
-	
-	// -----------------------------------------------------------------
-	
-	// 아이디 중복 확인
-	public boolean confirm(String id) throws SQLException {
-		con = null;
-		PreparedStatement ps =null;
-		ResultSet rs = null;
-		
-		
-		try {
-			con.prepareStatement("select id from jhp where id=?");
-			con = getQuestion();
+			con = DatabaseUtil.getConnection();
 			ps = con.prepareStatement(id);
 			ps.setString(1, id);
+			ps.setString(2, pass);
 			rs=ps.executeQuery();
 			if(rs.next()) {
-				result = 1;
+				if(rs.getString(1).equals(pass)) {
+					return 1; // 로그인 성공
+				}
+				else {
+					return 0; // 비밀번호 틀림
+				}
 			}
-		}catch(Exception e) {
-			
-		}finally {
-			try {
-				if(rs !=null) rs.close();
-				if(ps !=null) ps.close();
-				if(con !=null) con.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-	
-// -----------------------------------------------------------------
-	// 멤버 수정
-	
-	private Connection getQuestion() {
-		return null;
-		}
-
-	public int updateMember(Member vo) throws SQLException {
-		int result = 0;
-		try {
-			ps=con.prepareStatement("update user set pass=?,email=?,email1=?,contact=?,"
-					+ "question=?,answer=? where id=?");
-	
-			result = ps.executeUpdate();
-			
-		}catch(Exception e) {
-		}finally {
-			try {
-				if(ps !=null)ps.close();
-				if(con !=null)con.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
-	
-	// -----------------------------------------------------------------
-		// 아이디 중복 검사
-	public int idCheck(Connection con, String id) throws Exception,SQLException{
-		  PreparedStatement ps = null; 
-		  ResultSet rs = null;
-		  ps=con.prepareStatement("select id from member where id=?");
-		  
-		  try{
-		   ps = con.prepareStatement(id);
-		   ps.setString(1, id);
-		   rs = ps.executeQuery();
-		   if (rs.next()) {
-				return 1;
-			} else { // 아이디 없으면 0 반환
-				return 0;
-			}
-		} catch (Exception e) {
+			return -1; // 아이디 없을때
+		} catch(Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
 		}
-		// 오류 발생 시 -1 반환
-		return -1;
-	}
-	// -----------------------------------------------------------------
-			// 회원 가입
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	public List<Member> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Member selectOne(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void insertMember(Member member) {
-
+		return -2;
 		
 	}
-
-	public Connection getCon() {
-		return con;
-	}
-
-	public void setCon(Connection con) {
-		this.con = con;
-	}
-
-	public PreparedStatement getPs() {
-		return ps;
-	}
-
-	public void setPs(PreparedStatement ps) {
-		this.ps = ps;
-	}
-
-	public int getResult() {
-		return result;
-	}
-
-	public void setResult(int result) {
-		this.result = result;
-	}
 	
 	
 	
-
+	// ---------------------------------------------------------------------------------
+	// 회원 가입 
+	public int join(MemberDTO dto) {
+		try {
+			Connection con = DatabaseUtil.getConnection();
+			ps = con.prepareStatement("INSERT INTO user VALUES (?,?,?,?,?,?,?,?)");
+		
+			ps.setString(1, dto.getId());
+			ps.setString(2, dto.getPass());
+			ps.setString(3, dto.getName());
+			ps.setString(4, dto.getContact());
+			ps.setString(5, dto.getEmail());
+			ps.setString(6, dto.getQuestion());
+			ps.setString(7, dto.getAnswer());
+			return ps.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		return -1; // 회원가입 실패
+	}
+	// ---------------------------------------------------------------------------------
+	// 아이디 체크
+		public boolean idCheck(MemberDTO dto) {
+			try {
+				Connection con = DatabaseUtil.getConnection();
+				ps = con.prepareStatement("select id from user where id =?");
+				ps.setString(1, dto.getId());
+				rs = ps.executeQuery();
+				if(rs.next()) {
+					return rs.getBoolean(1);
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+				try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+				try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+			}
+			return false; // 회원가입 실패
+		}
+	
+		// ---------------------------------------------------------------------------------
+		// 회원 수정 
+	
+		public int modify(MemberDTO dto) {
+			try {
+				Connection con = DatabaseUtil.getConnection();
+				ps = con.prepareStatement("update user set user VALUES (?,?,?,?,?,?,?,?)");
+				ps.setString(1, dto.getId());
+				ps.setString(2, dto.getPass());
+				ps.setString(3, dto.getContact());
+				ps.setString(4, dto.getEmail());
+				ps.setString(5, dto.getQuestion());
+				ps.setString(6, dto.getAnswer());
+				return ps.executeUpdate();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+				try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+				try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+			}
+			return -1; // 회원수정 실패
+		}
+	
+		// ---------------------------------------------------------------------------------
+		// 아이디 찾기 
+			public int Forget_id(MemberDTO dto) {
+				try {
+					Connection con = DatabaseUtil.getConnection();
+					ps = con.prepareStatement("select id from user where id =?");
+					ps.setString(1, dto.getName());
+					ps.setString(2, dto.getContact());
+					ps.setString(3, dto.getEmail());
+					return ps.executeUpdate();
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}finally {
+					try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+					try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+					try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+				}
+				return -1; // 회원수정 실패
+			}
+// ---------------------------------------------------------------------------------
+		// 비밀번호 찾기 
+				public int Forget_pass(MemberDTO dto) {
+					try {
+						Connection con = DatabaseUtil.getConnection();
+						ps = con.prepareStatement("select pass from user where pass =?");
+						ps.setString(1, dto.getId());
+						ps.setString(2, dto.getName());
+						ps.setString(3, dto.getContact());
+						ps.setString(4, dto.getEmail());
+						ps.setString(5, dto.getQuestion());
+						ps.setString(6, dto.getAnswer());
+						return ps.executeUpdate();
+						
+					}catch(Exception e) {
+						e.printStackTrace();
+					}finally {
+						try {if(con != null) con.close();} catch(Exception e) {e.printStackTrace();}
+						try {if(ps != null) ps.close();} catch(Exception e) {e.printStackTrace();}
+						try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+					}
+					return -1; // 회원수정 실패
+				}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
