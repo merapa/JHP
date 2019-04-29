f22435a21ab5f1fb3442ed88e784ec9ce08b72b8
홍지원 <44790940+Gomgae@users.noreply.github.com>
2019-04-29 12:20:05 +0900

    Update JoinActionServlet.java

--- web/1/Servlet/JoinActionServlet.java
+++ web/1/Servlet/JoinActionServlet.java
@@ -11,18 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dto.MemberDTO;
+import util.DatabaseUtil;
 import dao.MemberDAO;
 
-@WebServlet(
-        name = "JoinActionServlet",
-        urlPatterns = {"/joinAction"},
-        loadOnStartup = 1
-)
+@WebServlet("/joinAction")
 
 @SuppressWarnings("serial")
 public class JoinActionServlet extends HttpServlet
 {
 	MemberDTO dto = new MemberDTO();	
+	MemberDAO dao = new MemberDAO();
 	
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -31,58 +29,67 @@ public class JoinActionServlet extends HttpServlet
 		response.setContentType("text/html;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         
+        
+        request.setCharacterEncoding("UTF-8");
+        
+        System.out.println("1");
 
-
-        dto.setId(request.getParameter("userID"));
+        dto.setId(request.getParameter("userId"));
         dto.setPass(request.getParameter("userPass"));
         dto.setName(request.getParameter("userName"));
         dto.setContact(request.getParameter("userContact"));
         dto.setEmail(request.getParameter("userEmail"));
-/*        dto.setQuestion(request.getParameter("userQuestion"));
-        dto.setAnswer(request.getParameter("userAnswer"));*/
+        dto.setQuestion(request.getParameter("userQuestion"));
+        dto.setAnswer(request.getParameter("userAnswer"));
 
-		HttpSession session  = request.getSession();
+		HttpSession session  = request.getSession();	
 		
-		
-        String userID = null;
-        
-        if(session.getAttribute("userID") != null ){
-            userID = (String) session.getAttribute("userID");
+        if(session.getAttribute("userId") != null ){
+            dto.setId ((String) session.getAttribute("userId"));
+
         }
         
-        if(userID != null){
+        System.out.println(dto.getId());
+        System.out.println(dto.getPass());
+        System.out.println(dto.getName());
+        System.out.println(dto.getContact());
+        System.out.println(dto.getEmail());
+        System.out.println(dto.getQuestion());
+        System.out.println(dto.getAnswer());
+        
+        if(dto.getId() != null){
             PrintWriter script = response.getWriter();
             script.println("<script>");
-            script.println("alert('이미 로그인 되어있습니다.')");
-            script.println("location.href = 'main.jsp'");
-            script.println("</script>");    
+            script.println("alert('성공 ')");
+            script.println("location.href = 'loginForm.jsp'");
+            script.println("</script>");  
         }
 
 	    if (dto.getId() == null || dto.getPass() == null || dto.getName() == null || dto.getEmail() == null) {
-            PrintWriter script = response.getWriter();
-            script.println("<script>");
-            script.println("alert('입력이 안 된 사항이 있습니다.')");
-            script.println("history.back()");
-            script.println("</script>");
-	    } else {
-            MemberDAO dao = new MemberDAO(); //인스턴스생성
-            int result = dao.join(dto);                
-
-            if(result == -1){ // 아이디가 기본키기. 중복되면 오류.
-                PrintWriter script = response.getWriter();
-                script.println("<script>");
-                script.println("alert('이미 존재하는 아이디 입니다.')");
-                script.println("history.back()");
-                script.println("</script>");
-            }
-            //가입성공
-            else {
-                PrintWriter script = response.getWriter();
-                script.println("<script>");
-                script.println("alert('회원가입 감사합니다.')");
-                script.println("location.href = 'main.jsp'");
-                script.println("</script>");
-            }
+	    	PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.print("alert('입력 안 된 사항이 있습니다');");
+			script.print("history.back();");
+			script.print("<script>"); 
+			System.out.println("2");
+		} else{
+			int result = dao.join(dto);
+			if(result == -1) {
+				PrintWriter script = response.getWriter();
+				DatabaseUtil.getConnection();
+				script.println("<script>");
+				script.print("alert('이미 존재하는 아이디 입니다.');");
+				script.print("history.back();");
+				script.print("<script>");
+				System.out.println("3");
+			}else {
+				PrintWriter script = response.getWriter();
+				script.println("<script>");
+				script.print("location.href='loginForm.jsp'");
+				script.print("<script>");
+				script.close();
+				System.out.println("4");
+			}
 	    }	
     }
 
